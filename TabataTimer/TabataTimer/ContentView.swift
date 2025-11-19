8c08491dda6bb178df3cfa59d0b359191a32d7b2
Ivan Revchuk <ivanrevchukpython@gmail.com>
2025-11-19 10:12:56 +0200

    chore: initial Xcode project

---
+++ TabataTimer/TabataTimer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TabataTimer
+//
+//  Created by Ivan Revchuk on 18.11.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TabataTimer/TabataTimer/TabataTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabataTimerApp.swift
+//  TabataTimer
+//
+//  Created by Ivan Revchuk on 18.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct TabataTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
